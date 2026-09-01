1f925fd1aebf90ceebcb4d6dee1b6dc6e11802d8
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-09-01 17:19:07 -0300

    Update lancamentos.js

--- js/pages/lancamentos.js
+++ js/pages/lancamentos.js
@@ -23,7 +23,7 @@ import { createModule } from "../engine/module.js";
 
 import {
     SCHEMA_LANCAMENTOS
-} from "../schemas/lancamentos.js";
+} from "./schemas/lancamentos.js";
 
 
 // ============================================================
