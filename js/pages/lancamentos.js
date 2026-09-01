20de1b0a606e7d43388d465cff391f337cc89513
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-09-01 14:54:20 -0300

    Fix import path for SCHEMA_LANCAMENTOS

--- js/pages/lancamentos.js
+++ js/pages/lancamentos.js
@@ -1,5 +1,5 @@
 import { createModule } from "../engine/module.js";
-import { SCHEMA_LANCAMENTOS } from "./schemas/lancamentos.js";
+import { SCHEMA_LANCAMENTOS } from "../schemas/lancamentos.js";
 
 export function iniciar() {
     return iniciarLancamentos();
