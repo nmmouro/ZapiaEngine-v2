4b1fd2c1a57c9f3d14730312394870d4952b9105
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-09-01 20:08:34 -0300

    Update empregados.js

--- js/pages/empregados.js
+++ js/pages/empregados.js
@@ -1,5 +1,5 @@
 import { createModule } from "../engine/module.js";
-import { SCHEMA_EMPREGADOS } from "../engine/schema.js";
+import { SCHEMA_EMPREGADOS } from "../schemas/empregados.js";
 
 export function iniciar() {
     return iniciarEmpregados();
