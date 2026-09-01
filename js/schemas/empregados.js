05024b3d929d5789953a131b012a06acaad42ec4
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-09-01 13:10:44 -0300

    Update empregados.js

--- js/schemas/empregados.js
+++ js/schemas/empregados.js
@@ -1,3 +1,17 @@
 import { SCHEMA_EMPREGADOS } from "../engine/schema.js";
+
 export { SCHEMA_EMPREGADOS };
+
+entity:
+        "empregados",
+
+
+    table:
+        "empregados",
+
+
+    title:
+        "Cadastro de Empregados",
+          
+          
 export default SCHEMA_EMPREGADOS;
